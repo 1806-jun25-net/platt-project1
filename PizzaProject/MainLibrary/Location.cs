3846502704e5cab500881b2e2b6b7b1dc6dfb09d
Phil <philipaplatt@gmail.com>
2018-07-02 16:44:08 -0400

    added dictionary called inventory to location class

--- PizzaProject/MainLibrary/Location.cs
+++ PizzaProject/MainLibrary/Location.cs
@@ -8,19 +8,45 @@ namespace MainLibrary
     {
         //fields 
 
-        int ingredientCount;
+
+        //the string will be the toppings ex "sausage" and the int will be the amount of that topping left. 
+        // assuming 1 per order. 
+
+        Dictionary<string, int> Inventory = new Dictionary<string, int>
+        {
+            {"Pepporoni", 5 },
+            {"Sausage", 5 },
+            {"Dough", 10},
+
+            
+
+        };
+       
+            
+        
+
+      
+      
+
         public string address { get; set; } = "Store 1";
   
 
         //constructor below. requires address or store name. 
         public Location(string address)
         {
-            ingredientCount = 3;
+            //ingredientCount = 3;
             this.address = address;
+           
         }
 
         public Location()
         {
+
+        }
+
+        public void removeInventory(string thingtoRemove)
+        {
+           
 
         }
 
--- PizzaProject/MainLibrary/Pizza.cs
+++ PizzaProject/MainLibrary/Pizza.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿//old way
+
+//public bool hasPepporoni { get; set; }
+//public bool hasOnions { get; set; }
+
+
+//public Pizza(string hasPepporoni, string hasOnions)
+//{
+//    if (hasPepporoni == "y")
+//        this.hasPepporoni = true;
+//    else
+//        this.hasPepporoni = false;
+//    if (hasOnions == "y")
+//        this.hasOnions = true;
+//    else
+//        this.hasOnions = false;
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,26 +27,13 @@ namespace MainLibrary
 
     public class Pizza
     {
-        //public bool hasPepporoni { get; set; }
-        //public bool hasOnions { get; set; }
-
-
-        //public Pizza(string hasPepporoni, string hasOnions)
-        //{
-        //    if (hasPepporoni == "y")
-        //        this.hasPepporoni = true;
-        //    else
-        //        this.hasPepporoni = false;
-        //    if (hasOnions == "y")
-        //        this.hasOnions = true;
-        //    else
-        //        this.hasOnions = false;
-        //}
+        
 
         public List<String> ListofToppings = new List<String>();
         decimal pizzaCost = 0;
         string pizzaSize = "medium";
 
+   
 
         public void addToppings(List<String> toppingList)
         {
@@ -39,8 +44,19 @@ namespace MainLibrary
 
         }
 
+        public void setPizzaSize (String size)
+        {
+            pizzaSize = size;
+
+        }
+
         public decimal calculatePizzaPrice()
         {
+
+
+            // instead of all the if elses here. could use a data structure (2d array or dictionary) to
+            //associate a topping or size for example with its price. Then run through a for each and just add
+            // the price section to the running total. 
             foreach (string topping in ListofToppings)
             {
                 if (topping == "Pepporoni")
